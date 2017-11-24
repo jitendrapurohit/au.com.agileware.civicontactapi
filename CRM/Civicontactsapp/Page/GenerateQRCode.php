910951e5ed0c25c2ff2f4194c25b2088a859dbb9
Alok Patel <alok@agileware.com.au>
2017-11-24 14:59:53 +0530

    CCA-27: Updated REST End-point URL.

--- CRM/Civicontactsapp/Page/GenerateQRCode.php
+++ CRM/Civicontactsapp/Page/GenerateQRCode.php
@@ -10,11 +10,16 @@ class CRM_Civicontactsapp_Page_GenerateQRCode extends CRM_Core_Page {
     $contact->id = $contactID;
     $contact->find(TRUE);
 
+    $config = CRM_Core_Config::singleton();
+    $restpath = $config->resourceBase . 'extern/rest.php';
+    $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+    $restendpoint = $protocol.$_SERVER['SERVER_NAME'].$restpath;
+
     $qr_code_pay_load = array(
       "contact_id"                => $contactID,
       "api_key"                   => $contact->api_key,
       "site_key"                  => CIVICRM_SITE_KEY,
-      "rest_end_point"            => CRM_Utils_System::url('civicrm/ajax/rest', '', TRUE),
+      "rest_end_point"            => $restendpoint,
     );
 
     QRcode::png(json_encode($qr_code_pay_load), FALSE, QR_ECLEVEL_H, 5, 3);
