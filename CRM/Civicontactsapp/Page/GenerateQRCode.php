1a4e0e6835c944fd8795b2d00e24b79534059269
Alok Patel <alok@agileware.com.au>
2017-12-12 16:33:32 +0530

    CCA-37: Restricting QR code image generation to logged in user.

--- CRM/Civicontactsapp/Page/GenerateQRCode.php
+++ CRM/Civicontactsapp/Page/GenerateQRCode.php
@@ -10,6 +10,12 @@ class CRM_Civicontactsapp_Page_GenerateQRCode extends CRM_Core_Page {
     $contact->id = $contactID;
     $contact->find(TRUE);
 
+    $session = CRM_Core_Session::singleton();
+    $isMe = ($contactID == $session->get('userID'));
+    if(!$isMe) {
+      throw new \Civi\API\Exception\UnauthorizedException('You\'re not authorized to view this page.');
+    }
+
     $config = CRM_Core_Config::singleton();
     $restpath = $config->resourceBase . 'extern/rest.php';
     $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
