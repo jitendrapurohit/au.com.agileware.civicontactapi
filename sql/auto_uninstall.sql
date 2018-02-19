0fd2b491bfccee206134b87471e2522c957faade
Alok Patel <alok@agileware.com.au>
2018-02-19 08:20:25 +0530

    CCA-100: Removing managed entity group on uninstalling extension.

--- sql/auto_uninstall.sql
+++ sql/auto_uninstall.sql
@@ -7,3 +7,5 @@ DROP TABLE IF EXISTS civicrm_cca_group_settings;
 DELETE FROM civicrm_custom_group WHERE name = 'CCA_Group_Settings';
 
 DELETE  FROM civicrm_option_group WHERE name = 'sync_to_cca';
+
+DELETE  FROM civicrm_group WHERE name = 'CiviCRM App Contacts';
