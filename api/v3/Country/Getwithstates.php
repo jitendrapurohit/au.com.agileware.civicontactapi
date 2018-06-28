5ebeb531503b9c4146674ee0efb9c7da40fde56e
Alok Patel <alok@agileware.com.au>
2018-06-28 16:23:26 +0530

    CCA-158: Added API to fetch countries and states.

---
+++ api/v3/Country/Getwithstates.php
@@ -0,0 +1,43 @@
+<?php
+use CRM_Civicontact_ExtensionUtil as E;
+
+/**
+ * Country.Getwithstates API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_country_Getwithstates_spec(&$spec) {
+
+}
+
+/**
+ * Country.Getwithstates API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_country_Getwithstates($params) {
+  $countries = civicrm_api3("Country", "get", array(
+    'options'    => array('limit' => 0),
+    'api.StateProvince.get' => array(
+        'options'    => array('limit' => 0),
+        'sequential' => FALSE,
+    ),
+  ));
+
+  foreach ($countries["values"] as $countryid => $country) {
+    $states = $country["api.StateProvince.get"]["values"];
+    foreach($states as $stateid => $state) {
+      $counties = CRM_Core_PseudoConstant::countyForState($stateid);
+      $countries["values"][$countryid]["api.StateProvince.get"]["values"][$stateid]["counties"] = $counties;
+    }
+  }
+
+  return $countries;
+}
